9233240fd6f286085df763dcd2e44b3b74939c70
eloquentcoder <eloquentintech@gmail.com>
2022-06-20 17:17:34 +0100

    Reduce queue time

--- app/Services/SendSubscriptionAlertEmail.php
+++ app/Services/SendSubscriptionAlertEmail.php
@@ -14,7 +14,7 @@ class SendSubscriptionAlertEmail {
     {
         Subscription::where('website_id', $website->id)->chunk(200, function($subscriptions) use($post, $website) {
             foreach ($subscriptions as $subscription) {
-                Mail::to($subscription->user->email)->later(now()->addMinute(), new SubscriptionAlert($post, $subscription->user, $website));
+                Mail::to($subscription->user->email)->later(now()->addSeconds(20), new SubscriptionAlert($post, $subscription->user, $website));
             }
         });
         
